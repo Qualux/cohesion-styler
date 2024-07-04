3f840eab5103faa81577e1343c1f31b240d74562
Casey Milne <casey@saberwp.com>
2024-07-04 22:13:43 +0300

    Init commit.

---
+++ Plugin.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*
+ * Plugin Name: Cohesion Styler
+ * Description: CSS styling block.
+ * Plugin URI: https://cohesionwp.com/plugins/cohesion-styler
+ * Author: Qualux
+ * Author URI: https://qualux.io
+ * Version: 0.0.1
+ */
+
+namespace CohesionStyler;
+
+define( 'COHESION_STYLER_URL', plugin_dir_url( __FILE__ ) );
+define( 'COHESION_STYLER_PATH', plugin_dir_path( __FILE__ ) );
+define( 'COHESION_STYLER_VERSION', '0.0.1' );
+
+class Plugin {
+
+    public function __construct() {
+
+
+    }
+
+}
+
+new Plugin();
